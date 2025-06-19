050c759003f102f354c3328c7a19faf87492874c
wastiz <valnos04@gmail.com>
2025-06-19 13:39:10 +0300

    asnother

--- src/modules/CsvGenerator.php
+++ src/modules/CsvGenerator.php
@@ -1,15 +1,35 @@
 <?php
 class CsvGenerator {
-  public function generate($filename, $data) {
-    if (empty($data)) return;
+    private $outputDir = 'output/';
 
-    $fp = fopen($filename, 'w');
-    fputcsv($fp, array_keys($data[0]));
-
-    foreach ($data as $row) {
-      fputcsv($fp, $row);
+    public function __construct() {
+        $this->ensureOutputDirectoryExists();
     }
 
-    fclose($fp);
-  }
+    private function ensureOutputDirectoryExists() {
+        if (!file_exists($this->outputDir)) {
+            mkdir($this->outputDir, 0777, true);
+        }
+    }
+
+    public function generate($filename, $data) {
+        if (empty($data)) {
+            return;
+        }
+
+        $filePath = $this->outputDir . $filename;
+        $fp = fopen($filePath, 'w');
+
+        fwrite($fp, "\xEF\xBB\xBF");
+
+        fputcsv($fp, array_keys($data[0]));
+
+        foreach ($data as $row) {
+            fputcsv($fp, $row);
+        }
+
+        fclose($fp);
+
+        return $filePath;
+    }
 }
